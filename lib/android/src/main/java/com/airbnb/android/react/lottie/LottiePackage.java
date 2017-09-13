8821cb36e26cda62ba4aecb3581f29b997c0e315
kyle <kyle@solidstategroup.com>
2017-09-13 18:01:59 +0100

    fix android build warning

--- lib/android/src/main/java/com/airbnb/android/react/lottie/LottiePackage.java
+++ lib/android/src/main/java/com/airbnb/android/react/lottie/LottiePackage.java
@@ -15,7 +15,6 @@ public class LottiePackage implements ReactPackage {
     return Collections.emptyList();
   }
 
-  // Deprecated RN 0.47
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
